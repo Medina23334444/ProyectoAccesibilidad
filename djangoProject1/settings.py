5a7b17a8582881fa0db6cc7eff71c56626f6a4cf
Medina23334444 <luis.a.medina.c@unl.edu.ec>
2026-05-23 22:28:32 -0500

    commit vulnerabilidad

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -22,31 +22,16 @@ env = environ.Env()
 # Leemos el archivo .env que está en la raíz de tu proyecto
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-# 3. ❌ ANTES tenías esto (Bórralo o coméntalo):
-# SECRET_KEY = 'django-insecure-xxxxxxxxxxxxxxxxx'
-
-# 4. ✅ AHORA usas esto (Seguridad Pro):
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
-# 5. Configurar el modo DEBUG desde el .env
 DEBUG = env.bool('DEBUG', default=False)
 
-# 6. Configurar la clave AES-256 para el cifrado de tus PDFs (Fase ABI)
-# Leemos la clave en base64 del .env y la convertimos a bytes
 AES_KEY_B64 = env('AES_ENCRYPTION_KEY', default=None)
 if AES_KEY_B64:
     AES_KEY = base64.b64decode(AES_KEY_B64)
 else:
     AES_KEY = None
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-oh$&t8v-fhra%4!wm-+ftzm1@6dd_!(1rux^+j8b%79$x8503t'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'django_backend', 'nginx_proxy']
 
